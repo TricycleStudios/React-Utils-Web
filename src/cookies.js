550e8b0cf124ac9916891321aa9930fb9ed57ec0
Scott Crespo <screspo@highcornergroup.com>
2015-07-23 10:55:21 -0400

    added cookies module

---
+++ src/cookies.js
@@ -0,0 +1,25 @@
+/*
+Useful javscript module for working with cookies.
+*/
+
+
+
+// Plain javascript to get a cookie by name. Useful to avoid using
+// JQuery's cookie plugin
+exports.getCookie = function(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(
+                cookie.substring(name.length + 1)
+                );
+              break;
+          }
+      }
+  }
+  return cookieValue;
+};
